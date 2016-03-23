0b2161275aba6849be668905d2084dabb8eab138
Unnikrishnan <krish8664@gmail.com>
2016-03-23 18:23:46 +0530

    Add css file as command line argument

--- src/markdown2docx/core.clj
+++ src/markdown2docx/core.clj
@@ -1,9 +1,14 @@
 (ns markdown2docx.core
   (:require [markdown2clj.core :as md]
-            [markdown2docx.docx-writer :as docx-writer]))
+            [markdown2docx.docx-writer :as docx-writer]
+            [markdown2docx.css :as css]))
 
-(defn -main [& [md-file docx-file]]
+(defn -main [& [md-file docx-file css-file]]
   (let [md-string (slurp md-file)
-        md-map (md/parse md-string)]
-    (when (.exists (docx-writer/write docx-file md-map))
+        md-map (md/parse md-string)
+        css-string (if (not (nil? css-file))
+                     (slurp css-file)
+                     "")
+        css-map (css/parse-css css-string)]
+    (when (.exists (docx-writer/write docx-file md-map css-map))
       (println "The docx file has been created @ " docx-file))))
